21580e25e52528d4f330f76c272ed72872326a03
iangodinho <maxian.godinho@gmail.com>
2024-10-20 00:05:55 -0300

    Added img folder and changed styles from menuLateral

--- src/components/MenuLateral/index.js
+++ src/components/MenuLateral/index.js
@@ -1,50 +1,69 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
-import styled from 'styled-components';
-
-const MenuWrapper = styled.div`
-  width: 250px;
-  height: 100vh;
-  background-color: #2c3e50;
-  color: white;
-  padding: 20px;
-`;
-
-const MenuItem = styled.div`
-  margin: 20px 0;
-  cursor: pointer;
-
-  &:hover {
-    background-color: #34495e;
-  }
-
-  &.active {
-    border-left: 5px solid gray;
-  }
-`;
-
-const StyledNavLink = styled(NavLink)`
-  color: white;
-  text-decoration: none;
-
-  &.active {
-    border-left: 5px solid gray;
-  }
-`;
+import * as C from './styles';
+import { useLocation } from 'react-router-dom';
+import logoMenu from '../../img/logo.png';
+import lockGrayIcon from '../../img/lock-gray.svg'; // Ícone cinza para Grupos
+import lockBlueIcon from '../../img/lock-blue.svg'; // Ícone azul para Grupos
+import gearGrayIcon from '../../img/gear-gray.svg'; // Ícone cinza para Configurações
+import gearBlueIcon from '../../img/gear-blue.svg'; // Ícone azul para Configurações
+import speedometerGrayIcon from '../../img/speedometer-gray.svg'; // Ícone cinza para Dashboard
+import speedometerBlueIcon from '../../img/speedometer-blue.svg'; // Ícone azul para Dashboard
 
 const MenuLateral = () => {
+  const location = useLocation(); // Hook do React Router para verificar a rota atual
+
+  // Verifica se o usuário está nas páginas
+  const isDashboardActive = location.pathname === '/dashboard';
+  const isGruposActive = location.pathname === '/grupos';
+  const isConfigActive = location.pathname === '/configuracoes';
+
   return (
-    <MenuWrapper>
-      <MenuItem>
-        <StyledNavLink to="/dashboard">Início</StyledNavLink>
-      </MenuItem>
-      <MenuItem>
-        <StyledNavLink to="/grupos">Grupos</StyledNavLink>
-      </MenuItem>
-      <MenuItem>
-        <StyledNavLink to="/configuracoes">Configurações</StyledNavLink>
-      </MenuItem>
-    </MenuWrapper>
+    <C.MenuWrapper>
+      <C.LogoWrapper>
+        <img src={logoMenu} alt="Logo" width="250" height="auto" />
+      </C.LogoWrapper>
+      <C.CategoryTitle>INÍCIO</C.CategoryTitle>
+      <C.MenuItem>
+        <C.StyledNavLink to="/dashboard">
+          <img
+            src={isDashboardActive ? speedometerBlueIcon : speedometerGrayIcon} // Ícone dinâmico para Dashboard
+            alt="Ícone de Dashboard"
+            width="20"
+            height="20"
+            style={{ marginRight: '10px' }}
+          />
+          Dashboard
+        </C.StyledNavLink>
+      </C.MenuItem>
+
+      <C.CategoryTitle>SEGURANÇA</C.CategoryTitle>
+      <C.MenuItem>
+        <C.StyledNavLink to="/grupos">
+          <img
+            src={isGruposActive ? lockBlueIcon : lockGrayIcon} // Ícone dinâmico para Grupos
+            alt="Ícone de Grupos"
+            width="20"
+            height="20"
+            style={{ marginRight: '10px' }}
+          />
+          Grupos
+        </C.StyledNavLink>
+      </C.MenuItem>
+
+      <C.CategoryTitle>SISTEMA</C.CategoryTitle>
+      <C.MenuItem>
+        <C.StyledNavLink to="/configuracoes">
+          <img
+            src={isConfigActive ? gearBlueIcon : gearGrayIcon} // Ícone dinâmico para Configurações
+            alt="Ícone de Configurações"
+            width="20"
+            height="20"
+            style={{ marginRight: '10px' }}
+          />
+          Configurações
+        </C.StyledNavLink>
+      </C.MenuItem>
+    </C.MenuWrapper>
   );
 };
 
