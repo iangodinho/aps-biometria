8879599f99f2dd67f81b87602a10e2f9621de62d
iangodinho <maxian.godinho@gmail.com>
2024-10-23 20:44:32 -0300

    Adjust style details on the groups page

--- src/components/ListaGrupos/index.js
+++ src/components/ListaGrupos/index.js
@@ -1,26 +1,128 @@
-import React from 'react';
-import styled from 'styled-components';
-
-const ListaWrapper = styled.div`
-  padding: 20px;
-`;
-
-const Grupo = styled.div`
-  margin: 10px 0;
-  padding: 10px;
-  border: 1px solid #ecf0f1;
-  border-radius: 4px;
-`;
+import React, { useState } from 'react';
+import {
+  TableWrapper,
+  TableContainer,
+  SearchBarWrapper,
+  SearchBar,
+  Table,
+  TableRow,
+  TableHeader,
+  TableCell,
+  Checkbox,
+  PaginationWrapper,
+  ArrowButton,
+  RowsPerPageSelector,
+  PaginationTextWrapper
+} from './styles'; // Importando os estilos
+import lupaIcon from '../../img/search.png'; // Importando a imagem da lupa
 
 const ListaGrupos = () => {
   const grupos = ['Administrador', 'Diretor', 'Analista', 'Suporte', 'TI'];
+  const [currentPage, setCurrentPage] = useState(1);
+  const [itemsPerPage, setItemsPerPage] = useState(3);
+  const [selectedItems, setSelectedItems] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+  const filteredGrupos = grupos.filter(grupo =>
+    grupo.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+  const totalPages = Math.ceil(filteredGrupos.length / itemsPerPage);
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentItems = filteredGrupos.slice(indexOfFirstItem, indexOfLastItem);
+
+  const handleNextPage = () => {
+    if (currentPage < totalPages) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
+
+  const handlePreviousPage = () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
+
+  const handleSelectAll = (e) => {
+    if (e.target.checked) {
+      setSelectedItems(currentItems.map((item) => item));
+    } else {
+      setSelectedItems([]);
+    }
+  };
+
+  const handleCheckboxChange = (grupo) => {
+    setSelectedItems((prevSelected) =>
+      prevSelected.includes(grupo)
+        ? prevSelected.filter((item) => item !== grupo)
+        : [...prevSelected, grupo]
+    );
+  };
+
+  const handleItemsPerPageChange = (e) => {
+    setItemsPerPage(parseInt(e.target.value));
+    setCurrentPage(1); // Reseta para a primeira página ao mudar o número de itens
+  };
 
   return (
-    <ListaWrapper>
-      {grupos.map((grupo, index) => (
-        <Grupo key={index}>{grupo}</Grupo>
-      ))}
-    </ListaWrapper>
+    <TableContainer>
+      <TableWrapper>
+        <SearchBarWrapper>
+          <img src={lupaIcon} alt="Buscar" style={{ marginRight: '10px', width: '20px' }} />
+          <SearchBar
+            type="text"
+            placeholder="Buscar..."
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+        </SearchBarWrapper>
+        <Table>
+          <thead>
+            <TableRow>
+              <TableHeader>
+                <Checkbox
+                  type="checkbox"
+                  onChange={handleSelectAll}
+                  checked={currentItems.length > 0 && selectedItems.length === currentItems.length}
+                />
+              </TableHeader>
+              <TableHeader>Nome</TableHeader>
+            </TableRow>
+          </thead>
+          <tbody>
+            {currentItems.map((grupo, index) => (
+              <TableRow key={index}>
+                <TableCell>
+                  <Checkbox
+                    type="checkbox"
+                    checked={selectedItems.includes(grupo)}
+                    onChange={() => handleCheckboxChange(grupo)}
+                  />
+                </TableCell>
+                <TableCell>{grupo}</TableCell>
+              </TableRow>
+            ))}
+          </tbody>
+        </Table>
+        <PaginationWrapper>
+          <PaginationTextWrapper>
+            <span>Linhas por página</span>
+            <RowsPerPageSelector onChange={handleItemsPerPageChange} value={itemsPerPage}>
+              <option value="3">3</option>
+              <option value="5">5</option>
+              <option value="7">7</option>
+              <option value="10">10</option>
+            </RowsPerPageSelector>
+          </PaginationTextWrapper>
+          <ArrowButton disabled={currentPage === 1} onClick={handlePreviousPage}>
+            &lt;
+          </ArrowButton>
+          <span>{currentPage} de {totalPages}</span>
+          <ArrowButton disabled={currentPage === totalPages} onClick={handleNextPage}>
+            &gt;
+          </ArrowButton>
+        </PaginationWrapper>
+      </TableWrapper>
+    </TableContainer>
   );
 };
 
---
+++ src/components/ListaGrupos/styles.js
@@ -0,0 +1,116 @@
+import styled from 'styled-components';
+
+export const TableContainer = styled.div`
+  display: flex;
+  margin-top: 50px;
+  flex-direction: column;
+  justify-content: center; /* Altera para 'flex-start' para evitar que a tabela fique muito baixa */
+  align-items: center;
+  padding: 20px;
+  max-height: 100vh;
+  background-color: #F0F2F5;
+  overflow: hidden; /* Removendo qualquer overflow desnecessário */
+`;
+
+export const SearchBarWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  width: 100%;
+  margin-bottom: 20px;
+  background-color: #fff;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  padding: 10px;
+`;
+
+export const SearchBar = styled.input`
+  border: none;
+  outline: none;
+  width: 100%;
+  font-size: 16px;
+  color: #B3B3B3;
+  ::placeholder {
+    color: #B3B3B3;
+  }
+  margin-left: 5px;
+`;
+
+export const TableWrapper = styled.div`
+  width: 60%;
+  background-color: #fff;
+  border-radius: 8px;
+  box-shadow: 0px 2px 10px rgba(0, 0, 0, 0.1);
+  padding: 20px;
+  margin-top: 0;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const Table = styled.table`
+  width: 100%;
+  border-collapse: collapse;
+`;
+
+export const TableRow = styled.tr`
+  &:nth-child(even) {
+    background-color: #f8f8f8;
+  }
+`;
+
+export const TableHeader = styled.th`
+  padding: 15px;
+  text-align: left;
+  background-color: #f5f5f5;
+  border-bottom: 2px solid #ddd;
+  font-weight: bold;
+  color: #6c757d;
+`;
+
+export const TableCell = styled.td`
+  padding: 15px;
+  border-bottom: 1px solid #ddd;
+  text-align: left;
+  color: #6c757d;
+`;
+
+export const Checkbox = styled.input`
+  width: 16px;
+  height: 16px;
+`;
+
+export const PaginationWrapper = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  margin-top: 20px;
+  color: #656565;
+`;
+
+export const ArrowButton = styled.button`
+  background: none;
+  border: none;
+  color: #656565;
+  font-size: 18px;
+  cursor: pointer;
+  margin: 0 10px;
+  &:disabled {
+    color: #ccc;
+    cursor: not-allowed;
+  }
+`;
+
+export const RowsPerPageSelector = styled.select`
+  padding: 5px;
+  border: none;
+  background-color: transparent;
+  color: #656565;
+  font-size: 16px;
+  cursor: pointer;
+  margin-left: 5px;
+`;
+
+export const PaginationTextWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  margin-right: 20px;
+`;
